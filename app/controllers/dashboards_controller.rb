c76cadba4ddb1266426145f667375e0fc5283239
KKevin Falck <kevin.falck@gmail.com>
2023-12-08 16:56:39 +0100

    fav_work

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
 
   def show
+    @current_page = 'dashboard'
     @my_workspaces = Workspace.where(user: current_user)
     @my_bookings = Booking.where(user: current_user)
     @bookings_of_my_workspaces = Booking.includes(:workspace).where(workspaces: { user: current_user })
