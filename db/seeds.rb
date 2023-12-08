245b4bda2793d44eace22664a40e09ecdb61e335
kiwismoothie <lauraberretta@hotmail.com>
2023-12-08 15:55:05 +0100

    dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -156,7 +156,7 @@ booking1 = Booking.new(
   rating: 5,
   start_date: "Thu, 30 Nov 2023",
   end_date: "Tue, 12 Dec 2023",
-  status: "accepted"
+  status: "pending"
 )
 booking1.save!
 
@@ -176,7 +176,7 @@ booking3 = Booking.new(
   rating: 5,
   start_date: "Thu, 30 Nov 2023",
   end_date: "Tue, 12 Dec 2023",
-  status: "accepted"
+  status: "pending"
 )
 booking3.save!
 
