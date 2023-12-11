44cc629493fd07fd50a16be94beefeae4dea74bd
kiwismoothie <lauraberretta@hotmail.com>
2023-12-11 14:36:59 +0100

    dashboard front ok

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
-class DashboardsController < ApplicationController
+class ProfilesController < ApplicationController
   def show
-    @current_page = 'dashboard'
+    @current_page = 'profile'
+    @user = current_user
     @my_workspaces = Workspace.where(user: current_user)
     @my_bookings = Booking.where(user: current_user)
     @bookings_of_my_workspaces = Booking.includes(:workspace).where(workspaces: { user: current_user })
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
   devise_for :users
   root to: "pages#home"
   get "intro", to: "pages#intro"
   get "dashboard", to: "dashboards#show"
+
+  resource :profile, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
