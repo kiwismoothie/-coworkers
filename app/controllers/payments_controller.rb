c8f5c844143a4a9970a4477dbbe6a85e25b05234
kiwismoothie <lauraberretta@hotmail.com>
2025-09-24 17:46:41 +0200

    ajout dans controller stripe publishable key

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,5 +2,7 @@ class PaymentsController < ApplicationController
   def new
     @booking = Booking.find(params[:booking_id])
     @workspace = @booking.workspace
+    @stripe_publishable_key = ENV['STRIPE_PUBLISHABLE_KEY']
   end
+
 end
